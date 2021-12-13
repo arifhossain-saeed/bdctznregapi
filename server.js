75e7efca252f865d1349641da85f915bc259fd28
arifhossain-saeed <arifhossain.saeed@hotmail.com>
2021-12-13 19:10:23 +0600

    updated api route

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ server.use(cors());
 
 
 // Server Routes - API Routes
-server.use("/bdctznreg/api/v1", apiRoutes);
+server.use("/api/v1", apiRoutes);
 
 // Default Error Handler when the requested route is non-existent
 server.use((req, res, next) => {
